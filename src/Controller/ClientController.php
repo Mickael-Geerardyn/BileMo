c8676c9b68958ed1afe4f2a6a4cdb7f7165b59da
Mickael-Geerardyn <root@DESKTOP-D1ECK70.localdomain>
2023-08-14 19:17:38 -0400

    add user delete by client feature

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,22 +4,29 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Services\CacheService;
+use App\Services\HttpExceptionEmptyData;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Psr\Cache\InvalidArgumentException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClientController extends AbstractController
 {
+
 	public function __construct(
-		private readonly SerializerInterface $serializer,
-		private readonly EntityManagerInterface $manager
+		private readonly SerializerInterface    $serializer,
+		private readonly EntityManagerInterface $manager,
+		private readonly UserRepository         $userRepository,
+		private readonly HttpExceptionEmptyData $httpExceptionEmptyData,
 	)
 	{
 	}
@@ -41,5 +48,33 @@ class ClientController extends AbstractController
 		$jsonUser = $this->serializer->serialize($user, 'json', $context);
 
 		return new JsonResponse($jsonUser, Response::HTTP_CREATED, json: true);
+	}
+
+	/**
+	 * @param Client $client
+	 * @param string $user_email
+	 *
+	 * @return JsonResponse
+	 * @throws HttpException
+	 */
+	#[Route('/api/client/{client_id}/user/{user_email}',
+		name: 'app_delete_client_user',
+		requirements: ['client_id' => '\d+', 'user_email' => '.+@.+\\..+'],
+		methods: ["DELETE"]
+	)]
+	#[ParamConverter('client', class: Client::class, options: ['id' => 'client_id'])]
+	public function deleteClientUser(
+		Client $client,
+		string $user_email
+	): JsonResponse
+	{
+		$user = $this->userRepository->findOneClientUser($user_email, $client);
+
+		$this->httpExceptionEmptyData->throwHttpExceptionIfEmptyData($this->httpExceptionEmptyData::NOT_FOUND_STATUT_CODE, $user);
+
+		$this->manager->remove($user);
+		$this->manager->flush();
+
+		return new JsonResponse(null, Response::HTTP_NO_CONTENT);
 	}
 }
